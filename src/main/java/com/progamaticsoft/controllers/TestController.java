6b2cdccb01c92dce0f8fba5a8cbf4c0db364432b
Pankaj <sachanpankaj.mca@gmail.com>
2024-07-21 21:50:39 +0530

    Signed-off-by: Pankaj <sachanpankaj.mca@gmail.com>

--- src/main/java/com/progamaticsoft/controllers/TestController.java
+++ src/main/java/com/progamaticsoft/controllers/TestController.java
@@ -8,6 +8,11 @@ import com.progamaticsoft.aop.logging.Profile;
 import com.progamaticsoft.config.controlleradvice.ResourceNotFoundException;
 import com.progamaticsoft.utils.EncryptionUtil;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 
@@ -17,6 +22,12 @@ public class TestController {
 	@Autowired
 	EncryptionUtil encryptionUtil;
 
+	@Operation(summary = "Testing API")
+//	@ApiResponses(value = {
+//			@ApiResponse(responseCode = "200", description = "Successful operation", 
+//			content = {
+//					@Content(mediaType = "application/json", schema = @Schema(implementation = String.class)) }),
+//			@ApiResponse(responseCode = "404", description = "Example not found") })
 	@Profile
 	@GetMapping("/test")
 	public String test(final HttpServletRequest request) {
