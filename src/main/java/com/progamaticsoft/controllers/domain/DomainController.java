6e61f38d82e8efefde5d88bee58369cc3a711a69
Pankaj <sachanpankaj.mca@gmail.com>
2024-08-23 14:29:29 +0530

    API documentation
    Signed-off-by: Pankaj <sachanpankaj.mca@gmail.com>

--- src/main/java/com/progamaticsoft/controllers/domain/DomainController.java
+++ src/main/java/com/progamaticsoft/controllers/domain/DomainController.java
@@ -6,19 +6,33 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.progamaticsoft.config.controlleradvice.ErrorResponse;
 import com.progamaticsoft.dtos.request.domain.DomainRequest;
 import com.progamaticsoft.dtos.response.domain.DomainResponse;
 import com.progamaticsoft.services.domain.DomainService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/domain")
 public class DomainController {
-	
+
 	@Autowired
 	private DomainService domainService;
 
+	@Operation(summary = "This API will create a new domain, and take DomainRequest object as request body")
+	@ApiResponses(value = { @ApiResponse(responseCode = "200", description = "Successful operation", content = {
+			@Content(mediaType = "application/json", schema = @Schema(implementation = DomainResponse.class)) }),
+			@ApiResponse(responseCode = "404", description = "Resource not found", content = {
+					@Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class)) }),
+	@ApiResponse(responseCode = "401", description = "Unauthorized access exception", content = {
+			@Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class)) })
+	})
 	@PostMapping("/create")
 	public DomainResponse crateDomain(@RequestBody @Valid final DomainRequest request) {
 		return domainService.createDomain(request);
