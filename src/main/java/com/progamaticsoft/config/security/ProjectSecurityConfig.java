6d3fcab28dd14fdf4352e177a6f5b0c8466042a8
Pankaj <sachanpankaj.mca@gmail.com>
2024-08-24 18:51:11 +0530

    security
    Signed-off-by: Pankaj <sachanpankaj.mca@gmail.com>

---
+++ src/main/java/com/progamaticsoft/config/security/ProjectSecurityConfig.java
@@ -0,0 +1,32 @@
+package com.progamaticsoft.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
+public class ProjectSecurityConfig {
+	@Bean
+	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests(
+				requests -> 
+				requests.requestMatchers("/api/admin/**").hasRole("ADMIN")
+				.anyRequest().authenticated()
+				);
+		http.csrf(AbstractHttpConfigurer::disable);
+//        http.addFilterBefore(new CustomLoggingFilter(),
+//                UsernamePasswordAuthenticationFilter.class);
+		// http.formLogin(withDefaults());
+//        http.httpBasic(withDefaults())
+		http.httpBasic(Customizer.withDefaults());
+		return http.build();
+	}
+
+}
