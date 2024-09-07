dee0ed5fc68df74275f9328f826f55fe0cc092b6
Pankaj <sachanpankaj.mca@gmail.com>
2024-09-07 20:36:03 +0530

    Project Security completed
    Signed-off-by: Pankaj <sachanpankaj.mca@gmail.com>

---
+++ src/main/java/com/progamaticsoft/config/security/DefaultDataCreator.java
@@ -0,0 +1,101 @@
+package com.progamaticsoft.config.security;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.progamaticsoft.database.entities.Role;
+import com.progamaticsoft.database.entities.User;
+import com.progamaticsoft.database.entities.enums.AppRole;
+import com.progamaticsoft.database.repositories.RoleRepository;
+import com.progamaticsoft.database.repositories.UserRepository;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Configuration
+public class DefaultDataCreator {
+
+	@Bean
+	CommandLineRunner initData(RoleRepository roleRepository, UserRepository userRepository,
+			PasswordEncoder passwordEncoder) {
+		log.info("****** Creating default user in the system......");
+		return args -> {
+			final Role userRole = roleRepository.findByRoleName(AppRole.ROLE_USER)
+					.orElseGet(() -> roleRepository.save(getRoleUser(AppRole.ROLE_USER)));
+
+			final Role adminRole = roleRepository.findByRoleName(AppRole.ROLE_ADMIN)
+					.orElseGet(() -> roleRepository.save(getRoleUser(AppRole.ROLE_ADMIN)));
+
+			if (!userRepository.existsByUserId("user1")) {
+				User user1 = getUser(passwordEncoder, userRole);
+				userRepository.save(user1);
+			}
+
+			if (!userRepository.existsByUserId("admin")) {
+				final User admin = getAdminUser(passwordEncoder, adminRole);
+				userRepository.save(admin);
+			}
+			log.info("****** Default users created in the system......");
+		};
+
+	}
+
+	private User getUser(PasswordEncoder passwordEncoder, Role userRole) {
+		User user1 = new User();
+		user1.setUserId("user1");
+		user1.setPassword(passwordEncoder.encode("password1"));
+		user1.setActive("Y");
+		user1.setDeleted("N");
+		user1.setFname("User");
+		user1.setBusinessId(0);
+		user1.setCreatedBy(0L);
+		user1.setAccountNonLocked(false);
+		user1.setAccountNonExpired(true);
+		user1.setCredentialsNonExpired(true);
+		user1.setEnabled(true);
+		user1.setCredentialsExpiryDate(LocalDate.now().plusYears(1));
+		user1.setAccountExpiryDate(LocalDate.now().plusYears(1));
+		user1.setTwoFactorEnabled(false);
+		user1.setSignUpMethod("email");
+		user1.setRole(userRole);
+		return user1;
+	}
+
+	private User getAdminUser(PasswordEncoder passwordEncoder, Role adminRole) {
+		User admin = new User();
+		admin.setUserId("admin");
+		admin.setPassword(passwordEncoder.encode("password1"));
+		admin.setActive("Y");
+		admin.setDeleted("N");
+		admin.setFname("Admin");
+		admin.setBusinessId(0);
+		admin.setCreatedBy(0L);
+		admin.setAccountNonLocked(false);
+		admin.setAccountNonExpired(true);
+		admin.setCredentialsNonExpired(true);
+		admin.setEnabled(true);
+		admin.setCredentialsExpiryDate(LocalDate.now().plusYears(1));
+		admin.setAccountExpiryDate(LocalDate.now().plusYears(1));
+		admin.setTwoFactorEnabled(false);
+		admin.setSignUpMethod("email");
+		admin.setRole(adminRole);
+		return admin;
+	}
+
+	private Role getRoleUser(AppRole approle) {
+		Role userRole = new Role();
+		userRole.setRoleName(approle);
+		userRole.setActive("Y");
+		userRole.setDeleted("N");
+		userRole.setCreatedate(new Date());
+		userRole.setModidate(new Date());
+		userRole.setCreatedBy(0L);
+		return userRole;
+	}
+
+}
--- src/main/java/com/progamaticsoft/config/security/ProjectSecurityConfig.java
+++ src/main/java/com/progamaticsoft/config/security/ProjectSecurityConfig.java
@@ -2,11 +2,7 @@ package com.progamaticsoft.config.security;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
-import java.time.LocalDate;
-import java.util.Date;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -14,7 +10,6 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -23,11 +18,6 @@ import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 import com.progamaticsoft.config.security.jwt.AuthEntryPointJwt;
 import com.progamaticsoft.config.security.jwt.AuthTokenFilter;
-import com.progamaticsoft.database.entities.Role;
-import com.progamaticsoft.database.entities.User;
-import com.progamaticsoft.database.entities.enums.AppRole;
-import com.progamaticsoft.database.repositories.RoleRepository;
-import com.progamaticsoft.database.repositories.UserRepository;
 
 @Configuration
 @EnableWebSecurity
@@ -37,33 +27,25 @@ public class ProjectSecurityConfig {
 	private AuthEntryPointJwt unauthorizedHandler;
 
 	@Bean
-	public AuthTokenFilter authenticationJwtTokenFilter() {
+	AuthTokenFilter authenticationJwtTokenFilter() {
 		return new AuthTokenFilter();
 	}
 
 	@Bean
-    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf ->
-                csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-                        .ignoringRequestMatchers("/api/auth/public/**")
-        );
-       // http.csrf(AbstractHttpConfigurer::disable);
-        http.authorizeHttpRequests((requests)
-                -> requests
-                .requestMatchers("/test").permitAll()
-                .requestMatchers("/swagger-ui/**","/swagger-ui.html","/v3/api-docs/**").permitAll()
-                .requestMatchers("/api/admin/**").hasRole("ADMIN")
-                .requestMatchers("/api/csrf-token").permitAll()
-                .requestMatchers("/api/auth/public/**").permitAll()
-                .anyRequest().authenticated());
-        http.exceptionHandling(exception
-                -> exception.authenticationEntryPoint(unauthorizedHandler));
-        http.addFilterBefore(authenticationJwtTokenFilter(),
-                UsernamePasswordAuthenticationFilter.class);
-        http.formLogin(withDefaults());
-        http.httpBasic(withDefaults());
-        return http.build();
-    }
+	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+		http.csrf(csrf -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+				.ignoringRequestMatchers("/api/auth/public/**"));
+		// http.csrf(AbstractHttpConfigurer::disable);
+		http.authorizeHttpRequests((requests) -> requests.requestMatchers("/test").permitAll()
+				.requestMatchers("/swagger-ui/**", "/swagger-ui.html", "/v3/api-docs/**").permitAll()
+				.requestMatchers("/api/admin/**").hasRole("ADMIN").requestMatchers("/api/csrf-token").permitAll()
+				.requestMatchers("/api/auth/public/**").permitAll().anyRequest().authenticated());
+		http.exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler));
+		http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+		http.formLogin(withDefaults());
+		http.httpBasic(withDefaults());
+		return http.build();
+	}
 
 	@Bean
 	AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
@@ -72,72 +54,7 @@ public class ProjectSecurityConfig {
 	}
 
 	@Bean
-	public PasswordEncoder passwordEncoder() {
+	PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
-	}
-
-	@Bean
-	public CommandLineRunner initData(RoleRepository roleRepository, UserRepository userRepository,
-			PasswordEncoder passwordEncoder) {
-		return args -> {
-			Role userRole = roleRepository.findByRoleName(AppRole.ROLE_USER)
-					.orElseGet(() -> roleRepository.save(getRoleUser(AppRole.ROLE_USER)));
-
-			Role adminRole = roleRepository.findByRoleName(AppRole.ROLE_ADMIN)
-					.orElseGet(() -> roleRepository.save(getRoleUser(AppRole.ROLE_ADMIN)));
-
-			if (!userRepository.existsByUserId("user1")) {
-				User user1 = new User();
-				user1.setUserId("user1");
-				user1.setPassword(passwordEncoder.encode("password1"));
-				user1.setActive("Y");
-				user1.setDeleted("N");
-				user1.setFname("USER NAME1");
-				user1.setBusinessId(0);
-				user1.setCreatedBy(0L);
-				user1.setAccountNonLocked(false);
-				user1.setAccountNonExpired(true);
-				user1.setCredentialsNonExpired(true);
-				user1.setEnabled(true);
-				user1.setCredentialsExpiryDate(LocalDate.now().plusYears(1));
-				user1.setAccountExpiryDate(LocalDate.now().plusYears(1));
-				user1.setTwoFactorEnabled(false);
-				user1.setSignUpMethod("email");
-				user1.setRole(userRole);
-				userRepository.save(user1);
-			}
-
-			if (!userRepository.existsByUserId("admin")) {
-				User admin = new User();
-				admin.setUserId("admin");
-				admin.setPassword(passwordEncoder.encode("password1"));
-				admin.setActive("Y");
-				admin.setDeleted("N");
-				admin.setFname("USER NAME1");
-				admin.setBusinessId(0);
-				admin.setCreatedBy(0L);
-				admin.setAccountNonLocked(false);
-				admin.setAccountNonExpired(true);
-				admin.setCredentialsNonExpired(true);
-				admin.setEnabled(true);
-				admin.setCredentialsExpiryDate(LocalDate.now().plusYears(1));
-				admin.setAccountExpiryDate(LocalDate.now().plusYears(1));
-				admin.setTwoFactorEnabled(false);
-				admin.setSignUpMethod("email");
-				admin.setRole(adminRole);
-				userRepository.save(admin);
-			}
-		};
-	}
-
-	private Role getRoleUser(AppRole approle) {
-		Role userRole = new Role();
-		userRole.setRoleName(approle);
-		userRole.setActive("Y");
-		userRole.setDeleted("N");
-		userRole.setCreatedate(new Date());
-		userRole.setModidate(new Date());
-		userRole.setCreatedBy(0L);
-		return userRole;
 	}
 }
